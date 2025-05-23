8572b011f5c723a476fa91c2ad9b4a76c5681425
Armando Mora <armandomora14115@gmail.com>
2025-05-23 09:43:27 -0500

    Rutas dinamicas y navegacion programática, funcionando correctamente

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,48 @@ import "./App.css";
 import Dashboard from "./Dashboard/Dashboard";
 import Header from "./Header/Header";
 import Reviews from "./Reviews/Reviews";
-import AboutMe from "./AboutMe/AboutMe";
-import AboutUs from "./AboutUs/AboutUs";
 
-//rutas hijos
+//rutas padre e hijos
+import AboutMe from "./AboutMe/AboutMe"; //ruta padre
 import Contact from "./AboutMe/about-me-files/Contact";
 import Hobbies from "./AboutMe/about-me-files/Hobbies";
 import MyStory from "./AboutMe/about-me-files/MyStory";
+
+import AboutUs from "./AboutUs/AboutUs"; //ruta padre
 import SiteHistory from "./AboutUs/about-us-files/SiteHistory";
 import SiteMission from "./AboutUs/about-us-files/SiteMission";
+import { useEffect, useState } from "react";
+import Review from "./Review/Review";
 
 function App() {
+  const [reviews, setReviews] = useState([]); // Nueva variable useState
+
+  useEffect(() => {
+    // Obtiene los datos de reseñas del servidor.
+    fetch("https://emoji-critic.es.tripleten-services.com/v1/reviews")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // Pasa el cuerpo de la respuesta analizada a la función setter.
+        setReviews(data);
+      })
+      .catch(console.error);
+    // Un array de dependencia vacío significa que el hook sólo se ejecuta cuando
+    // se carga el componente.
+  }, []);
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/reviews" element={<Reviews reviews={reviews} />}/>
+          <Route
+            path="/reviews/:reviewId"
+            element={<Review reviews={reviews} />}
+          />
+        
         <Route path="/about-me" element={<AboutMe />}>
           <Route path="contact" element={<Contact />} />
           <Route path="hobbies" element={<Hobbies />} />
